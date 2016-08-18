93732dbc6709ff143319b9f109d346c430d15d56
Tracy Chou <tracy.chou@cs.stanford.edu>
2016-08-18 00:38:06 -0400

    Create basic task list with strikethrough toggle.

--- todo.js
+++ todo.js
@@ -1,4 +1,45 @@
+var TaskList = React.createClass({
+    render: function() {
+      var taskItems = [
+        <TaskItem key={0} description={"Learn React"}/>,
+        <TaskItem key={1} description={"Learn Swift"}/>
+      ];
+
+      return (
+        <div className="taskList">
+          {taskItems}
+        </div>
+      );
+    }
+  });
+
+var TaskItem = React.createClass({
+    getInitialState: function() {
+      return {
+        done: false
+      }
+    },
+
+    render: function() {
+      var style = this.state.done ?
+        {textDecoration: "line-through"} :
+        {};
+      var onClick = evt => {
+        this.setState({done: !this.state.done});
+      };
+
+      return (
+        <div
+          onClick={onClick}
+          style={style}
+          className="taskItem">
+          {this.props.description}
+        </div>
+      );
+    }
+  });
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <TaskList />,
   document.getElementById('example')
 );
